9e0ee462560a51f0f3870f2009304ad2590d3ce3
Asthenesis <irfan.20fari@gmail.com>
2022-11-01 14:18:54 +0700

    Trying to fix

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -19,18 +19,16 @@ export default function Posts() {
   return (
     <main>
       <h1>Posts</h1>
+      <Link to="admin" className="text-red-600 underline">
+          Admin
+      </Link>
       <ul>
         {posts.map((post) => (
           <li key={post.slug}>
-            <Link
-              to={post.slug}
-              className="text-blue-600 underline"
-            >
+            <Link to={post.slug} className="text-blue-600 underline">
               {post.title}
             </Link>
-            <Link to="admin" className="text-red-600 underline">
-              Admin
-            </Link>
+            
           </li>
         ))}
       </ul>
